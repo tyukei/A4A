0f1600fbcfac67c59d802d8b08f9245f61457a2c
Keita Nakata <keitanakata.developer@gmail.com>
2025-12-23 13:35:33 +0900

    Update __init__.py

--- agent_4_agent/subagents/__init__.py
+++ agent_4_agent/subagents/__init__.py
@@ -3,6 +3,7 @@ from .surfer_agent import surfer_agent
 from .searcher_agent import searcher_agent
 from .reviewer_agent import reviewer_agent
 from .pm_final_report_agent import pm_final_report_agent
+from .tool_creater_agent import tool_creater_agent
 
 __all__ = [
     "creater_agent",
@@ -10,4 +11,5 @@ __all__ = [
     "searcher_agent",
     "reviewer_agent",
     "pm_final_report_agent",
+    "tool_creater_agent",
 ]
